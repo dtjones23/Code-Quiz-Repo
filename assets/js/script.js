e2484569e38875083c85232fe05bd2b144a35f66
DeAngelo Jones <deangelojones0708@gmail.com>
2024-02-02 17:02:35 -0600

    added game html, css, and variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Quiz questions
+const questionsEl = document.getElementById('questions');
+const timerEl = document.getElementById('time');
+const optionEl = document.getElementById('options');
+const submitBtn = document.getElementById('submit');
+const startBtn = document.getElementById('start');
+const initialsEl = document.getElementById('initials');
+const feedbackEl = document.getElementById('feedback');
+
+// Start Quiz / Timer
+// Get Questions
+// Question submit
+// End Quiz / Highscore
